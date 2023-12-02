6d0e91dbb174360a8f5e6093334741ed8d7df3a6
Ilya Opyakin <i.opyakin@outlook.com>
2023-12-02 20:36:54 +0100

    First part is done

--- Day 01/day01.py
+++ Day 01/day01.py
@@ -8,13 +8,29 @@ def parse_file(file_to_process):
 
     return data
 
+
+def get_digits(s):
+    digits = "".join(filter(str.isdigit, s))
+    if digits.__len__() > 0:
+        res = digits[0] + digits[digits.__len__() - 1]
+    else:
+        res = None
+    
+    return int(res)
+
 def main():
     
-    file_name = "Day 01\day01.txt"
+    file_name = "Day 01\day01-prd.txt"
     file_data = parse_file(file_name)
-    print(file_data)
+
+    res_part_one = 0
+    for line in file_data:
+        calibration_values = get_digits(line)
+        res_part_one += calibration_values
+
+    
     print("----------------------------")
-    print("Part One:", 1)
+    print("Part One:", res_part_one)
     print("Part Two:", 2)
 
 if __name__ == "__main__":
