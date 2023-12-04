0bcb5bb56fbc8dd70033e9362ab757f4e1e1c5c9
Ilya Opyakin <i.opyakin@outlook.com>
2023-12-04 21:38:53 +0100

    Part one has been solved

--- Day 02/day02.py
+++ Day 02/day02.py
@@ -19,25 +19,40 @@ def parse_data(data):
 
     return result
 
-    
-
-def main():
+def is_game_possible(games):
     game_set =  {
         'red':  12,
         'green':13,
         'blue': 14
     }
 
-    file_name = "Day 02\day01-dev.txt"
-    file_data = parse_file(file_name)
-    game_counter = 1
-    for line in file_data:
-        res = parse_data(line)
-        print(game_counter, res)
-        game_counter+=1
+    for game in games:
+        if  ("red"   in game and game["red"]   > game_set["red"]  ) or \
+            ("green" in game and game["green"] > game_set["green"]) or \
+            ("blue"  in game and game["blue"]  > game_set["blue"] ):
+            return False
+
+    return True
+
+def main():
+
 
     res_part_one = 0
     res_part_two = 0
+
+
+    file_name = "Day 02\day02-prd.txt"
+    file_data = parse_file(file_name)
+    game_counter = 1
+    for line in file_data:
+        game = parse_data(line)
+        print(game_counter, game)
+        if is_game_possible(game):
+            res_part_one+=game_counter
+        
+        game_counter+=1
+
+
 
 
     print("----------------------------")
