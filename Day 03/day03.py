c9ada03eebdc80f1b0fbf0da2fb2343d063155d9
Ilya Opyakin <i.opyakin@outlook.com>
2023-12-06 21:17:32 +0100

    Dev task for part one does work

--- Day 03/day03.py
+++ Day 03/day03.py
@@ -1,7 +1,41 @@
-def parse_file(file_to_process):
-    file = open(file_to_process, mode='r')
-    data: list[str] = file.read().split('\n')
-    return data
+# --- Day 3: Gear Ratios ---
+import re
+from typing import Literal
+
+
+class Part_number:
+    def __init__(self, value, line_num, start, end):
+        self.value = value
+        self.line_num = line_num
+        self.start = start
+        self.end = end
+    
+
+
+def parse_file(file_to_process, target: Literal['digits', 'symbols']):
+    results = []
+    with open(file_to_process, 'r') as file:
+        for line_number, line_content in enumerate(file, start=0):
+            if target == 'digits':
+                res = re.findall(r'\d+', line_content)
+            if target == 'symbols':
+                res = re.findall(r'[^0-9.\n]', line_content)
+
+            for value in res:
+                start_position = line_content.index(value)
+                end_position = start_position + len(value) - 1
+
+                if target == 'symbols':
+                    value = -1
+
+                obj = Part_number(int(value), line_number, start_position, end_position)
+                results.append(obj)
+
+    return results
+
+def check_nearby_fields(p):
+    print(p)
+    return True
 
 
 def main():
@@ -9,8 +43,20 @@ def main():
     res_part_one = 0
     res_part_two = 0
 
-    file_name = 'Day 02\day02-prd.txt'
-    file_data = parse_file(file_name)
+    file_name = 'Day 03\day03-dev.txt'
+
+    part_numbers = parse_file(file_name,'digits')
+    symbols      = parse_file(file_name,'symbols')
+    #check_nearby_fields(part_numbers[0],)
+
+
+    for part in part_numbers:
+        for symbol in symbols:
+            if abs(part.line_num - symbol.line_num) <= 1:
+                if symbol.start >= part.start - 1 and symbol.end <= part.end + 1:
+                    res_part_one += part.value
+                    #print(part.value)
+                    continue
 
     print('----------------------------')
     print('Part One:', res_part_one)
