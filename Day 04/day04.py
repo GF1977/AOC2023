f720ff082f300c9f1e00525a1a0716a739d68919
Ilya Opyakin <i.opyakin@outlook.com>
2023-12-07 21:42:44 +0100

    Part two done, need to optimize

--- Day 04/day04.py
+++ Day 04/day04.py
@@ -13,8 +13,11 @@ def main():
     res_part_one = 0
     res_part_two = 0
 
+    cards_pile = {}
 
-    for line in file_data:
+    for line_number, line in enumerate(file_data, start=1):
+        if  line_number not in cards_pile:
+            cards_pile[line_number] = 1
         tmp_split = line.split('|')
         card_part_one = tmp_split[0].split(':')[1]
         card_part_two = tmp_split[1]
@@ -23,7 +26,17 @@ def main():
         win_numbers = re.findall(r'\d+',card_part_two)
 
         win_match = set(elf_numbers) & set(win_numbers)
+        winners_count = win_match.__len__()
         res_part_one += int(pow(2, win_match.__len__()-1))
+
+        current_line_value = cards_pile[line_number]
+        for i in range(line_number + 1, line_number + winners_count + 1):
+            if i not in cards_pile:
+                cards_pile[i] = current_line_value + 1
+            else:
+                cards_pile[i] = cards_pile[i] + current_line_value
+
+    res_part_two = sum(cards_pile.values())
 
     print("----------------------------")
     print("Part One:", res_part_one)
