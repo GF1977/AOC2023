817ed0e141508b1577523d4e51fa891277c9508e
Ilya Opyakin <i.opyakin@outlook.com>
2023-12-07 20:53:29 +0100

    Part one has been completed, it was easy

--- Day 04/day04.py
+++ Day 04/day04.py
@@ -7,12 +7,23 @@ def parse_file(file_to_process):
 
 def main():
     
-    file_name = "Day 01\day04-prd.txt"
+    file_name = "Day 04\day04-prd.txt"
     file_data = parse_file(file_name)
 
     res_part_one = 0
     res_part_two = 0
 
+
+    for line in file_data:
+        tmp_split = line.split('|')
+        card_part_one = tmp_split[0].split(':')[1]
+        card_part_two = tmp_split[1]
+
+        elf_numbers = re.findall(r'\d+',card_part_one)
+        win_numbers = re.findall(r'\d+',card_part_two)
+
+        win_match = set(elf_numbers) & set(win_numbers)
+        res_part_one += int(pow(2, win_match.__len__()-1))
 
     print("----------------------------")
     print("Part One:", res_part_one)
@@ -21,5 +32,5 @@ def main():
 if __name__ == "__main__":
     main()
 
-#Part One: 
+#Part One: 20855
 #Part Two: 
