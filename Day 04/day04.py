9a6875e1ee847cd1919faaaf4bc558c63ffc68b3
Ilya Opyakin <i.opyakin@outlook.com>
2023-12-07 21:55:42 +0100

    Optimized version of part two

--- Day 04/day04.py
+++ Day 04/day04.py
@@ -1,4 +1,5 @@
 import re
+import timeit
 
 def parse_file(file_to_process):
     file = open(file_to_process, mode="r")
@@ -6,7 +7,6 @@ def parse_file(file_to_process):
     return data
 
 def main():
-    
     file_name = "Day 04\day04-prd.txt"
     file_data = parse_file(file_name)
 
@@ -16,25 +16,22 @@ def main():
     cards_pile = {}
 
     for line_number, line in enumerate(file_data, start=1):
-        if  line_number not in cards_pile:
-            cards_pile[line_number] = 1
-        tmp_split = line.split('|')
-        card_part_one = tmp_split[0].split(':')[1]
-        card_part_two = tmp_split[1]
+        cards_pile[line_number] = cards_pile.get(line_number, 1)
+        card_part_one = line.split('|')[0].split(':')[1]
+        card_part_two = line.split('|')[1]
 
         elf_numbers = re.findall(r'\d+',card_part_one)
         win_numbers = re.findall(r'\d+',card_part_two)
 
-        win_match = set(elf_numbers) & set(win_numbers)
-        winners_count = win_match.__len__()
-        res_part_one += int(pow(2, win_match.__len__()-1))
+        win_match = set(elf_numbers).intersection(win_numbers)
+        winners_count = len(win_match)
+
+        if winners_count > 0:
+            res_part_one += 1 << (winners_count - 1)
 
         current_line_value = cards_pile[line_number]
         for i in range(line_number + 1, line_number + winners_count + 1):
-            if i not in cards_pile:
-                cards_pile[i] = current_line_value + 1
-            else:
-                cards_pile[i] = cards_pile[i] + current_line_value
+            cards_pile[i] = cards_pile.get(i, 1) + current_line_value
 
     res_part_two = sum(cards_pile.values())
 
@@ -43,7 +40,10 @@ def main():
     print("Part Two:", res_part_two)
 
 if __name__ == "__main__":
+    start_time = timeit.default_timer()
     main()
+    end_time = timeit.default_timer()
+    print("Elapsed time:", end_time - start_time)
 
 #Part One: 20855
-#Part Two: 
+#Part Two: 5489600
