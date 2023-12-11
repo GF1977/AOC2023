b3b7950725996ca9f5ee8f68ca2cfac921a3c53a
Ilya Opyakin <i.opyakin@outlook.com>
2023-12-11 12:17:48 +0100

    Part two is fully done

--- Day 05/day05.py
+++ Day 05/day05.py
@@ -15,87 +15,88 @@ def get_intersection(obj_start, obj_range, map):
     else:
         return [start + (map.dst_range_start - map.src_range_start), end - start - 1]
 
+def get_mapping_for_part_one(map,source):
+    for X in map:
+        if source >= X.src_range_start and source < X.src_range_start + X.range_len:
+           return [X.dst_range_start + source - X.src_range_start]
+   
+    return [source]
+
+def get_mapping_for_part_two(maps,source):
+    new_mapping = []
+    for map in maps:
+        intersection = get_intersection(obj_start = source[0], obj_range = source[1], map = map) 
+        if intersection is not None:
+            new_mapping.append(intersection)
+
+    if len(new_mapping) == 0:
+        return [source]
+
+    return new_mapping
+
 def get_mapping(result, map_name, source, puzzle_part = 1):
-    global_map = []
+    maps = []
     for S in source:
-        maps = []
-        #---------------
         if puzzle_part == 1:
-            res = -1
-            for X in result[map_name]:
-                if S >= X.src_range_start and S < X.src_range_start + X.range_len:
-                    res = X.dst_range_start + S - X.src_range_start
-                if res < 0:
-                    res = S    
-        #---------------
+            maps += get_mapping_for_part_one(result[map_name], source = S)
+
         if puzzle_part == 2:
-            for map in result[map_name]:
-                intersection = get_intersection(obj_start = S[0], obj_range = S[1], map = map) 
-                if intersection is not None:
-                    maps.append(intersection)
-                res = S
-        #---------------
-        if len(maps) == 0:
-            maps.append(res)
-        global_map += maps
-    return global_map
+            maps += get_mapping_for_part_two(result[map_name], source = S)
+
+    return maps
 
 def parse_file(file_to_process):
-    file = open(file_to_process, mode="r")
-    data: list[str] = file.read().split("\n")
+    file = open(file_to_process, mode='r')
+    data: list[str] = file.read().split('\n')
     return data
 
 def parse_data(file_data):
     seeds = []
     mapping_name = []
-    result = {}
+    maps = {}
 
-    name = None
     for line in file_data:
-        if "seeds:" in line:
-            seeds = list(map(int, line.split(":")[1].split()))
+        if 'seeds:' in line:
+            seeds = list(map(int, line.split(':')[1].split()))
         elif line.strip() == '':
             continue
         elif ':' in line:
-            name, values = line.split(':')
-            result[name] = set()
+            name, [] = line.split(':')
+            maps[name] = set()
             mapping_name.append(name)
         else:
-            data = list(map(int, line.split()))
-            result[name].add(SeedMapping(data[0], data[1], data[2]))
+            src, dst, range = list(map(int, line.split()))
+            maps[name].add(SeedMapping(src, dst, range))
             
-    return seeds, result, mapping_name
-
+    return seeds, maps, mapping_name
 
 def main():
-    
-    file_name = "Day 05\day05-prd.txt"
+    file_name = 'Day 05\day05-prd.txt'
     file_data = parse_file(file_name)
     seeds, result, mapping_name = parse_data(file_data)
 
-    tmp_res_one = seeds
-    tmp_res_two = [[seeds[i], seeds[i+1]] for i in range(0, len(seeds), 2)]
+    tmp_res_one = seeds #seeds one by one
+    tmp_res_two = [[seeds[i], seeds[i+1]] for i in range(0, len(seeds), 2)] #seeds range
     for name in mapping_name:
         tmp_res_one = get_mapping(result, name, tmp_res_one, puzzle_part=1)
         tmp_res_two = get_mapping(result, name, tmp_res_two, puzzle_part=2)
 
-    lowest_location = float('inf')
-    for l_n in tmp_res_two:
-        if l_n[0] < lowest_location:
-            lowest_location = l_n[0]
-
     res_part_one = min(tmp_res_one)
-    res_part_two = lowest_location
 
-    print("----------------------------")
-    print("Part One:", res_part_one)
-    print("Part Two:", res_part_two)
+    res_part_two = float('inf')
+    for l_n in tmp_res_two:
+        if l_n[0] < res_part_two:
+            res_part_two = l_n[0]
 
-if __name__ == "__main__":
+    print('----------------------------')
+    print('Part One:', res_part_one)
+    print('Part Two:', res_part_two)
+
+if __name__ == '__main__':
     start_time = timeit.default_timer()
     main()
     end_time = timeit.default_timer()
-    print("Elapsed time:", end_time - start_time)
+    print('Elapsed time:', end_time - start_time)
 
 #Part One: 486613012
 #Part Two: 56931769
