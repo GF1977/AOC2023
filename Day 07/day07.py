6499761920a2449c07d4dc90f55e3999ba81f345
Ilya Opyakin <i.opyakin@outlook.com>
2023-12-24 17:25:11 +0100

    Another option, letters via numbers - still doesn't work

--- Day 07/day07.py
+++ Day 07/day07.py
@@ -1,128 +1,144 @@
-import re
 import timeit
 import collections
+from collections import Counter
+
 
 def parse_file(file_to_process):
     file = open(file_to_process, mode="r")
     data: list[str] = file.read().split("\n")
     return data
 
+
 def convert_to_hand(hand_list):
-    res = ''
-    replacement = {'666':'A', '555':'K', '444':'Q', '333':'J', '222':'T', '109':'9', '108':'8', '107':'7', '106':'6', '105':'5', '104':'4', '103':'3', '102':'2'}
+    res = ""
+    replacement = {
+        "A": "A",
+        "K": "B",
+        "Q": "C",
+        "J": "D",
+        "T": "E",
+        "9": "F",
+        "8": "G",
+        "7": "H",
+        "6": "I",
+        "5": "J",
+        "4": "K",
+        "3": "L",
+        "2": "M",
+    }
     for x in hand_list:
-        card = str(x).rstrip('0')
+        card = str(x).rstrip("0")
         if card in replacement:
             card = replacement[card]
-            res+=(card)
+            res += card
         else:
-            print('Stop')
+            print("Stop")
     return res
 
+
 def get_combination(input):
-    replacement = {'A':666, 'K':555, 'Q':444, 'J':333, 'T':222}
     counter = collections.Counter(input)
     res = []
 
     for char, count in counter.items():
-        if char in replacement:
-            char = replacement[char]
-        else:
-            char = '10'+char
-        res.append([int(char)*10**(count), count])
+        res.append([char, count])
 
-    card_value_list = []
-    for r in res:
-        for count in range(0,r[1]):
-            #card_value_list.append(r[0]*(10**(r[1])))
-            card_value_list.append(r[0])
-    card_value_list.sort(reverse=True)
-    nice_hand = convert_to_hand(card_value_list)
-    card_value_str = ''
-    for x in card_value_list:
-         tmp = str(x)
-         card_value_str+=tmp
-    card_value = int(card_value_str)
+    nice_hand = new_sorting(convert_to_hand(input))
 
+    card_value = 0
+    len_res = len(res)
 
-    if len(res) == 5:
-        return ["High card", card_value, nice_hand]
-    if len(res) == 4:
-        return ["One pair", card_value, nice_hand]
-    if len(res) == 3:
+    if len_res == 5:  # "High card"
+        return [0, card_value, nice_hand]
+
+    if len_res == 4:  # "One pair"
+        return [1, card_value, nice_hand]
+
+    if len_res == 3:
         for card in res:
-            if card[1]==3:
-                return ["Three of a kind", card_value, nice_hand]
-            if card[1]==2:
-                return ["Two pair", card_value, nice_hand]
-    if len(res) == 2:
+            if card[1] == 2:  # "Two pair"
+                return [3, card_value, nice_hand]
+
+            if card[1] == 3:  # "Three of a kind"
+                return [4, card_value, nice_hand]
+
+    if len_res == 2:
         for card in res:
-            if card[1]==4:
-                return ["Four of a kind", card_value, nice_hand]
-            if card[1]==3:
-                return ["Full house", card_value, nice_hand]
-    if len(res) == 1:
-        return ["Five of a kind", card_value, nice_hand]
+            if card[1] == 3:  # "Full house"
+                return [5, card_value, nice_hand]
+
+            if card[1] == 4:  # Four of a kind"
+                return [6, card_value, nice_hand]
+
+    if len_res == 1:  # "Five of a kind"
+        return [7, card_value, nice_hand]
+
 
 def get_result(hands, combination, i):
     res = 0
     for hand in hands:
         if hand[2] == combination:
-            #if combination == 'Full house':
-            print(hand[3])
-            res += hand[1]*i
-            i+=1
+            res += hand[1] * i
+            i += 1
     return res, i
 
-def main():
 
-    hands = []
-    
-    file_name = "Day 07\day07-prd.txt"
+def new_sorting(hand):
+    # Count the frequency of each character
+    freq = Counter(hand)
+
+    # Sort the characters first by frequency (desc), then by character (asc)
+    sorted_chars = sorted(freq.items(), key=lambda x: (-x[1], x[0]))
+
+    # Join the characters back into a string
+    sorted_string = "".join([char * count for char, count in sorted_chars])
+
+    return sorted_string
+
+
+def main():
+    hands = {}
+    hands_with_values = {}
+
+    file_name = "Day 07\\day07-prd.txt"
     file_data = parse_file(file_name)
     for f in file_data:
-        x = f.split(' ')[0]
-        y = int(f.split(' ')[1])
+        x = f.split(" ")[0]
+        hand_value = int(f.split(" ")[1])
         z = get_combination(x)
+        key = z[0]
+        nice_hand = z[2]
 
+        if key not in hands:
+            hands[key] = [nice_hand]
+        else:
+            hands[key].append(nice_hand)
 
-        hands.append([z[1], y, z[0], z[2]])
-
-    hands.sort()
-    #print(hands)
+        if nice_hand not in hands_with_values:
+            hands_with_values[nice_hand] = hand_value
 
     res_part_one = 0
-    i = 1
-    tmp, i = get_result(hands,'High card',i)
-    res_part_one+=tmp
-    print(tmp)
-    tmp, i = get_result(hands,'One pair',i)
-    res_part_one+=tmp
-    print(tmp)
-    tmp, i = get_result(hands,'Two pair',i)
-    res_part_one+=tmp
-    print(tmp)
-    tmp, i = get_result(hands,'Three of a kind',i)
-    res_part_one+=tmp
-    print(tmp)
-    tmp, i = get_result(hands,'Full house',i)
-    res_part_one+=tmp
-    print(tmp)
-    tmp, i = get_result(hands,'Four of a kind',i)
-    res_part_one+=tmp
-    print(tmp)
-    tmp, i = get_result(hands,'Five of a kind',i)
-    res_part_one+=tmp
-    print(tmp)
 
+    print("----------")
+    cnt = 1
+    for i in range(0, 7):
+        tpm_res = 0 
+        if i in hands:
+            x = hands[i]
+            x.sort(reverse=True)
+            for hand in x:
+                #print(i, hand, hands_with_values[hand])
+                tpm_res = cnt * hands_with_values[hand]
+                res_part_one += tpm_res
+                cnt += 1
+        print(tpm_res)
 
-    
     res_part_two = 0
-
 
     print("----------------------------")
     print("Part One:", res_part_one)
     print("Part Two:", res_part_two)
+
 
 if __name__ == "__main__":
     start_time = timeit.default_timer()
@@ -130,11 +146,12 @@ if __name__ == "__main__":
     end_time = timeit.default_timer()
     print("Elapsed time:", end_time - start_time)
 
-#Part One: 
+# Part One:
 
+# 248469601 - nope
 # 248755486 - nope
 # 248806739 too high
 # 249019601 too high
 # 253201326
 # 253201326
-#Part Two: 
+# Part Two:
