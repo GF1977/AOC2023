bd311d9d3c8df18ad2832c60d6f5146f52cdbe98
Ilya Opyakin <i.opyakin@outlook.com>
2023-12-30 21:49:33 +0100

    Day 09 - vanilla

---
+++ Day 09/day09.py
@@ -0,0 +1,29 @@
+import timeit
+
+
+def parse_file(file_to_process: str) -> list[str]:
+    with open(file_to_process, mode="r") as file:
+        data = file.read().split("\n")
+    return data
+
+
+def main():
+    file_name = "Day 09\\day09-dev.txt"
+    file_data = parse_file(file_name)
+
+    res_part_one = 0
+    res_part_two = 0
+
+    print("----------------------------")
+    print("Part One:", res_part_one)
+    print("Part Two:", res_part_two)
+
+
+if __name__ == "__main__":
+    start_time = timeit.default_timer()
+    main()
+    end_time = timeit.default_timer()
+    print("Elapsed time:", end_time - start_time)
+
+# Part One:
+# Part Two:
