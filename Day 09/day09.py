87592c8ead8514b1c009a293692050c262345b9c
Ilya Opyakin <i.opyakin@outlook.com>
2024-01-05 15:32:12 +0100

    Both parts work!

--- Day 09/day09.py
+++ Day 09/day09.py
@@ -8,35 +8,24 @@ def parse_file(file_to_process: str) -> list[str]:
     return data
 
 
-def get_coeff_old(seq_len: int) -> list[int]:
-    if seq_len == 1:
-        return [1]
-    else:
-        prev = get_coeff(seq_len - 1)
-        return (
-            [prev[0]]
-            + [
-                (-1) ** (i) * (abs(prev[i - 1]) + abs(prev[i]))
-                for i in range(1, len(prev))
-            ]
-            + [0 + abs(prev[-1])]
-        )
-    
 def get_coeff(n):
     row = [1]
     for i in range(n):
-        row.append(-1 ** (i+1) * row[i] * (n-i) // (i+1))
-    row[-1]=abs(row[-1])
+        row.append(-(1 ** (i + 1)) * row[i] * (n - i) // (i + 1))
+    row[-1] = abs(row[-1])
     return row
+
 
 def get_solutioin(input, part):
     res = 0
     for value in input:
-        #coef = get_coeff_old(len(value) + (2 - part))
         coef = get_coeff(len(value))
         if part == 2:
-           #value.pop(-1)
-           coef.pop(0)
+            if len(value) % 2 == 1:
+                coef = get_coeff(len(value) + (-1))
+                value.pop(-1)
+            coef.pop(0)
+
         x = 0
         i = 0
         for v in value:
@@ -48,7 +37,7 @@ def get_solutioin(input, part):
 
 
 def main():
-    file_name = "Day 09\\day09-dev.txt"
+    file_name = "Day 09\\day09-prd.txt"
     file_data = parse_file(file_name)
 
     input = []
